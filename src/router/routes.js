9ce4a62b62a19b5483329af305178f2f0dc6eb58
muriloleal13 <msleal@inf.ufsm.br>
2025-04-11 17:14:17 -0300

    fix(netlify): old pages import

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,14 +2,7 @@ const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [
-      { path: "", component: () => import("pages/IndexPage.vue") },
-      { path: "login", component: () => import("pages/LoginPage.vue") },
-      {
-        path: "search/:search",
-        component: () => import("pages/SearchPage.vue"),
-      },
-    ],
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
 
   // Always leave this as last one,
